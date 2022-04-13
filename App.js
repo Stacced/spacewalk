2866ae199921676122be3576a9483b3a0d03deb0
Stacked at work <20029433+Stacced@users.noreply.github.com>
2022-04-13 17:57:04 +0200

    Initial commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Spacewalk - your rocket launches & space news companion</Text>
       <StatusBar style="auto" />
     </View>
   );
