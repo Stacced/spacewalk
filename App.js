d8f6904341775e0a85aa2d56fe212f2717e58949
Stacked at work <20029433+Stacced@users.noreply.github.com>
2022-04-26 17:39:21 +0200

    feat: basic tab navigation, react redux dependencies

--- App.js
+++ App.js
@@ -1,13 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import Home from './components/Home';
+import Calendar from './components/Calendar';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Spacewalk - your rocket launches & space news companion</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused ? 'ios-home' : 'ios-home-outline';
+          } else if (route.name === 'Calendar') {
+            iconName = focused ? 'ios-calendar' : 'ios-calendar-outline';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: 'light-blue',
+        tabBarInactiveTintColor: 'gray',
+      })}>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Calendar" component={Calendar} />
+      </Tab.Navigator>
+      <StatusBar/>
+    </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({
---
+++ components/Home.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Spacewalk - your rocket launches & space news companion</Text>
+        </View>
+    )
+}
+
+export default Home;
