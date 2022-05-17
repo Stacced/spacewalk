e35fa8544cbe9604297bf9fc431140d1ffa44df1
Stacked at work <20029433+Stacced@users.noreply.github.com>
2022-05-17 16:22:51 +0200

    fix(redux): forgot to add news api to store and middlewares

--- redux/store.js
+++ redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import favoritesSlice from './favoritesSlice';
 import { launchesApi } from './launchesApi';
+import { newsApi } from './newsApi';
 
 export const store = configureStore({
     reducer: {
         favorites: favoritesSlice.reducer,
         [launchesApi.reducerPath]: launchesApi.reducer,
+        [newsApi.reducerPath]: newsApi.reducer,
     },
-    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(launchesApi.middleware),
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(launchesApi.middleware).concat(newsApi.middleware),
 });
